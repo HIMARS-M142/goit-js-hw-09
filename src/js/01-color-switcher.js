3d238ea19fd5ae9d0f672a503b0b5dc49aa7bf74
HIMARS-M142 <maksympona@gmail.com>
2023-07-22 20:08:22 +0200

    upd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+const body = document.body;
+let intervalId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(e) {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+}
+
+function onStopBtnClick(e) {
+  clearInterval(intervalId);
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+}
